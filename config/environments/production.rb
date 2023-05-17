ff9f1e673210c018eee165346d6a126df79496d0
vaibhavbajaj <vaibhavbajaj15@gmail.com>
2023-05-17 16:25:26 -0500

    changed email credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "vaibhavwaste96@gmail.com",
-    :password             => "cezdflcumsxplsqu",
+    :user_name            => "eventnxtapp@gmail.com",
+    :password             => "ubpzqicdatyvbjoi",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
